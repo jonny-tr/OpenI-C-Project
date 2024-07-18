f27a22336b63ff86d909cf4d40273e7aae84b516
Jonny <50047576+jonny-tr@users.noreply.github.com>
2024-07-18 15:03:05 -0500

    Command_word is now a linked list

--- assembler.h
+++ assembler.h
@@ -40,29 +40,31 @@ typedef struct macro_t {
 } macro_t;
 typedef macro_t *macro_ptr;
 
-
 typedef struct symbols_list {
-    char *name; /*label*/
-    int counter; /*IC or NULL*/
-    char *type; /*data/external/entry*/
+    char *name; /* label */
+    int counter; /* IC or NULL */
+    char *type; /* data/external/entry */
     struct symbols_list *next;
 } symbols_list;
 typedef symbols_list *symbols_ptr;
 
 typedef struct variable_t {
     char *content; 
-    int counter; /*DC*/
+    int counter; /* DC */
     struct variables_list *next;
 } variable_t;
 typedef variable_t *variable_ptr;
 
-typedef struct {
-    unsigned int are : 3; /*bits 02-00*/
-    unsigned int dest_addr : 4; /*bits 06-03*/
-    unsigned int src_addr : 4;  /*bits 10-07*/
-    unsigned int opcode : 4;  /*bits 14-11*/
+typedef struct command_word {
+    unsigned int are : 3; /* bits 02-00 */
+    unsigned int dest_addr : 4; /* bits 06-03 */
+    unsigned int src_addr : 4;  /* bits 10-07 */
+    unsigned int opcode : 4;  /* bits 14-11 */
+    unsigned int l : 3;
+    struct command_word *next;
 } command_word;
-   
+typedef command_word *command_ptr;
+
 /*---------------------------------functions---------------------------------*/
 /* text utils */
 int as_strdup(char **dest, const char *s);
@@ -71,7 +73,7 @@ int is_valid_command(char *command);
 int read_next_line(FILE *fd, char **line);
 int read_next_word(const char line[], int *position, char **next_part);
  /*add from shahar's changes*/
-int binstr_to_octal(char *line);
+ int command_to_num(command_word cmd);
 
 /* pre_assembler */
 int pre_assembler(char **in_fd, macro_ptr macro_table_head);
@@ -87,18 +89,17 @@ int get_word(char *position, char *word);
 int get_word_type(char *position);
 
 
-/*phase_one*/
+/* phase_one */
 int is_valid_label(char *word, symbols_ptr symbols_table_head, macro_ptr macro_table_head);
 void remove_colon(char *label);
 void add_symbol(symbols_ptr *head, char *name, int counter, char *type);
+int calc_l(command_word *field, int cmnd);
 
 /* phase_two */
-int build_ent(FILE *ent_fd, symbol_ptr symbol_table);
-int build_ob(FILE *ob_fd, char *filename, int ic, int dc);
-int phase_two(FILE *fd, char *filename, symbol_ptr symbol_table, int ext_ic,
+int build_ent(FILE *ent_fd, symbols_ptr symbol_table);
+int build_ob(FILE *ob_fd, commant_ptr command_head, int ic, int dc);
+int phase_two(FILE *fd, char *filename, symbols_ptr symbol_table, int ext_ic,
               int dc);
-int entry_update(symbol_ptr symbol_table, char *word);
-
-
+int entry_update(symbols_ptr symbol_table, char *word);
 
 #endif /* ASSEMBLER_H */
